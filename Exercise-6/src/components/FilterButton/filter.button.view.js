3a5d985a5b4c6aa0721dc6f94099c001956cdfbc
Luka Tarman <83535200+lukatarman@users.noreply.github.com>
2022-12-12 16:15:15 +0100

    Delete Exercise-6 directory

--- Exercise-6/src/components/FilterButton/filter.button.view.js
+++
@@ -1,9 +0,0 @@
-const FilterButton = ({ name, handleClick }) => {
-  return (
-    <button className="button-filter m-1" onClick={handleClick}>
-      {name}
-    </button>
-  );
-};
-
-export default FilterButton;
